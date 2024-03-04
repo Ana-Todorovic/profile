6c6b79feb727e66f7e6482d6b05cb140b450587d
Ana Todorovic <anatodorovic@Anas-MacBook-Air.local>
2024-03-03 19:01:01 -0800

    beginning of profile site, header, footer, intro video, about section

---
+++ animationLoad.js
@@ -0,0 +1,6 @@
+let theElement = document.getElementsByClassName('typewrite')[0];
+theElement.addEventListener("DOMContentLoaded", (element) => {element.classList.add('w3-container w3-center w3-animate-left')});
+
+
+
+document.addEventListener("DOMContentLoaded", () => document.getElementsByClassName = "w3-container w3-center w3-animate-left")
