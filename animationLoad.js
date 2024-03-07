dc21dde35d793d3c2346b2f8bf79e36f81c3ba7e
Ana Todorovic <anatodorovic@Anas-MacBook-Air.local>
2024-03-06 18:46:06 -0800

    smooth scroll

--- animationLoad.js
+++ animationLoad.js
@@ -1,6 +1,15 @@
-let theElement = document.getElementsByClassName('typewrite')[0];
-theElement.addEventListener("DOMContentLoaded", (element) => {element.classList.add('w3-container w3-center w3-animate-left')});
+let options = {
+    rootMargin: "0px",
+    threshold: 1.0,
+};
 
+function onView(entries) {
+    if (entries && entries[0].isIntersecting) {
+        document.getElementById("process").classList.add("w3-container", "w3-center", "w3-animate-left");
+    }
+}
 
-
-document.addEventListener("DOMContentLoaded", () => document.getElementsByClassName = "w3-container w3-center w3-animate-left")
+let myCoolElement = document.getElementById("process");
+console.log(myCoolElement);
+let myCoolObserver = new IntersectionObserver(onView, options);
+myCoolObserver.observe(myCoolElement);
